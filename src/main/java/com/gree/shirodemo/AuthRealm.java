f0dd454110ba7db54368c288ea0c275208f56759
zys1123 <987986567@qq.com>
2019-03-23 08:38:39 +0800

    安全框架搭建

---
+++ src/main/java/com/gree/shirodemo/AuthRealm.java
@@ -0,0 +1,55 @@
+package com.gree.shirodemo;
+
+import com.gree.shirodemo.entity.Permission;
+import com.gree.shirodemo.entity.Role;
+import com.gree.shirodemo.entity.User;
+import com.gree.shirodemo.service.UserService;
+import org.apache.shiro.authc.*;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+public class AuthRealm extends AuthorizingRealm {
+    @Autowired
+    private UserService userService;
+    //授权
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        User user = (User) principalCollection.fromRealm(this.getClass().getName()).iterator().next();
+        List<String> permissionList = new ArrayList<>();
+        List<String> roleNameList = new ArrayList<>();
+        Set<Role> roleSet = user.getRoles();
+        if (roleSet != null) {
+            for(Role role : roleSet) {
+                roleNameList.add(role.getRname());
+                Set<Permission> permissionSet = role.getPermissions();
+                if (permissionSet != null) {
+                    for (Permission permission : permissionSet) {
+                        permissionList.add(permission.getName());
+                    }
+                }
+            }
+        }
+        SimpleAuthorizationInfo info = new SimpleAuthorizationInfo();
+        info.addStringPermissions(permissionList);
+        info.addRoles(roleNameList);
+        return info;
+    }
+
+    //认证登录
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+
+        UsernamePasswordToken usernamePasswordToken = (UsernamePasswordToken) authenticationToken;
+        String username = usernamePasswordToken.getUsername();
+        User user = userService.findByUserName(username);
+        return new SimpleAuthenticationInfo(user,user.getPassword(),this.getClass().getName());
+
+    }
+}
